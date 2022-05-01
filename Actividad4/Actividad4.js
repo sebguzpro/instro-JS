28a89b27af6d369d3e80be0f8557f4fe28860afd
Sebastián G.Pro <94193169+sebguzpro@users.noreply.github.com>
2022-05-01 14:11:07 -0500

    Update Actividad4.js

--- Actividad4/Actividad4.js
+++ Actividad4/Actividad4.js
@@ -1,19 +1,21 @@
 //1. numero y multiplos de 5 con while
 var elnu = prompt('Elige un número y te mostraré los múltiplos del 5 que sean menores.')  
-m5 = 0
-while (m5 < elnu) {
-    console.log(m5)
-    m5=m5+5
+m5 = 1
+while (m5 <= elnu) {
+    if(m5%5===0){
+    console.log(m5)}
+    m5++
 } 
 
 //2. lo mismo pero con do while
 var elnu = prompt('Elige un número y te mostraré los múltiplos del 5 que sean menores.')  
 var m5 = 0
 do {
-    console.log(m5)
-    m5=m5+5
-}
-while (m5 < elnu)
+    if(m5%5===0){
+    console.log(m5)}
+    m5++
+} 
+while (m5 <= elnu) 
 
 //3. for
 var contador = 0
