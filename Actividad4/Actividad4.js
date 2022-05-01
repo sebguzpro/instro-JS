442b7b61e816efd8088fb053f95a6c5ff258ea08
Sebastián G.Pro <94193169+sebguzpro@users.noreply.github.com>
2022-05-01 13:56:07 -0500

    Create Actividad4.js

---
+++ Actividad4/Actividad4.js
@@ -0,0 +1,26 @@
+//1. numero y multiplos de 5 con while
+var elnu = prompt('Elige un número y te mostraré los múltiplos del 5 que sean menores.')  
+m5 = 0
+while (m5 < elnu) {
+    console.log(m5)
+    m5=m5+5
+} 
+
+//2. lo mismo pero con do while
+var elnu = prompt('Elige un número y te mostraré los múltiplos del 5 que sean menores.')  
+var m5 = 0
+do {
+    console.log(m5)
+    m5=m5+5
+}
+while (m5 < elnu)
+
+//3. for
+var contador = 0
+for (var index = 1; index <= 50; index++){
+if(!(index%2==0)){
+    contador = contador + 1
+    console.log(`${index} es impar`)
+ }
+}
+console.log(`Del 1 al 50 hay ${contador} números impares`)
